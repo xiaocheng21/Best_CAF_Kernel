e2593892e5e44e36ee16e04717462cac706f07a1
xiaocheng21 <3579367084@qq.com>
2024-06-16 19:22:35 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-KERNEL_SOURCE=https://github.com/RahulPalXDA/kernel_samsung_j2y18lte.git
+KERNEL_SOURCE=https://github.com/xiaocheng21/kernel_oppo_sdm8250.git
 BRANCH=stock
-KERNEL_VERSION=msm-3.18
-PLATFORM=*-89xx.0
+KERNEL_VERSION=msm-4.19
+PLATFORM=*-SMxx50
